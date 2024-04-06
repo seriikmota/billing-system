be26889f456b297ba838380d850efafc0b345a99
Erik Mota <seriikmota.dev@gmail.com>
2024-04-05 21:08:28 -0300

    Swagger title

--- src/main/java/com/ueg/probweb/billingsystem/BillingSystemApplication.java
+++ src/main/java/com/ueg/probweb/billingsystem/BillingSystemApplication.java
@@ -1,8 +1,11 @@
 package com.ueg.probweb.billingsystem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "API para Vendas", description = "Produzido por: Erik de Sousa Mota e Davi Faria Gonçalves"))
 @SpringBootApplication
 public class BillingSystemApplication {
 
